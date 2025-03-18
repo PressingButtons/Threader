45e9bd6c16425ecc3d9185d1083ebff2bf31c2c8
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2025-03-18 12:48:55 -0500

    changed send to be a one-way action in home, added request function to be for promises

--- thread.ts
+++ thread.ts
@@ -6,10 +6,10 @@ type ThreadMessage = {
 export class ThreadObject {
 
     protected _listeners:{[key:string]: Function}
-    protected _thread:Worker;;
+    protected _thread:Worker;
 
-    constructor(thread:Worker) {
-        this._thread = thread;
+    constructor(url:URL|string) {
+        this._thread = new Worker(url, {type:'module'});
         this._listeners = { };
         this._thread.addEventListener('message',this._onmessage.bind(this));
     }
@@ -27,51 +27,19 @@ export class ThreadObject {
         delete this._listeners[route];
     }
 
-    send(route:string, content:any, transferables:Transferable[] = []) {
+    request(route:string, content:any, transferables:Transferable[] = []) {
         return new Promise((resolve, reject) => {
             this._thread.addEventListener('error', reject);
             this._thread.addEventListener('message', (event) => {
                 const message:ThreadMessage = event.data;
                 if(message.route == route) resolve(message.content);
             });
-            this._thread.postMessage({route, content}, transferables);
+            this.send(route, content, transferables);
         });
-    }
-}
-
-export class SharedThread {
-
-    protected _listeners:{[key:string]: Function}
-    protected _thread:SharedWorker
-
-    constructor(thread:SharedWorker) {
-        this._thread = thread;
-        this._listeners = { };
-        this._thread.port.addEventListener('message',this._onmessage.bind(this));
-    }
-
-    private _onmessage(event:MessageEvent) {
-        const {route, content} = event.data;
-        if(this._listeners[route]) this._listeners[route](content);
-    }
-
-    listen(route:string, method:Function) {
-        this._listeners[route] = method;
-    }
-
-    deafen(route:string) {
-        delete this._listeners[route];
     }
 
     send(route:string, content:any, transferables:Transferable[] = []) {
-        return new Promise((resolve, reject) => {
-            this._thread.port.addEventListener('error', reject);
-            this._thread.port.addEventListener('message', (event) => {
-                const message:ThreadMessage = event.data;
-                if(message.route == route) resolve(message.content);
-            });
-            this._thread.port.postMessage({route, content}, transferables);
-        });
+        this._thread.postMessage({route, content}, transferables);
     }
 }
 
@@ -105,42 +73,4 @@ export class WorkerThread {
     send(route:string, content:any) {
         this._thread.postMessage({route, content});
     }
-}
-
-export class SharedWorkerThread {
-
-    protected _listeners:{[key:string]: Function}
-    protected _port:MessagePort;
-
-    constructor( port:MessagePort ) {
-        this._port = port;
-        this._listeners = { };
-        this._port.addEventListener('message',this._onmessage.bind(this));
-        this._port.start( );
-    }
-
-    get port( ) {
-        return this._port;
-    }
-
-    private async _onmessage(event:MessageEvent) {
-        const {route, content} = event.data;
-        if(this._listeners[route]) {
-            const result = await this._listeners[route](content);
-            this.send(route, result);
-        }
-    }
-
-    listen(route:string, method:Function) {
-        this._listeners[route] = method;
-    }
-
-    deafen(route:string) {
-        delete this._listeners[route];
-    }
-
-    send(route:string, content:any) {
-        this._port.postMessage({route, content});
-    }
-
 }
